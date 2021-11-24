c3b072755567e9eeaefe53af47b5a91eccadd17a
Franco-Ou <franoubel@hotmail.com>
2021-11-24 17:19:39 -0300

    Desafío terminado

--- app.js
+++ app.js
@@ -1,11 +1,47 @@
+const Contenedor = require('./contenedor.js');
 const express = require('express');
-
 const app = express();
 
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 8080);
+
+const getAllItems = async() => {
+    const contenedorDeProductos = new Contenedor("productos.txt");
+    await contenedorDeProductos.initializer();
+    const productsArray = contenedorDeProductos.getAll();
+    return productsArray;
+}
+
+const getRandomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+const getRandomProduct = async() => {
+    const contenedorDeProductos = new Contenedor("productos.txt")
+    await contenedorDeProductos.initializer();
+    const productsArray = contenedorDeProductos.getAll();
+    const randomProduct = productsArray[getRandomNumber(0, productsArray.length)];
+
+    return randomProduct;
+}
 
 app.get('/', (req, res) => {
-    console.log('GET request');
+    res.send("<h1>Página principal</h1>");
+})
 
-    res.send("<h1>Prueba</h1>")
+app.get('/productos', async(req, res) => {
+    try {
+        const allItems = await getAllItems();
+        res.send(`<p>${JSON.stringify(allItems)}</p>`);
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+app.get('/productoRandom', async(req, res) => {
+    try {
+        const randomProduct = await getRandomProduct();
+        res.send(`<p>${JSON.stringify(randomProduct)}</p>`);
+    } catch (error) {
+        console.log(error);
+    }
 })
